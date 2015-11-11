bf28a6be30293185c42d68b29f9071bbf868aa9c
kwonhee <s10hotkkh@nate.com>
2015-11-11 19:34:47 +0900

    hihi

--- cmd/go-getting-started/main.go
+++ cmd/go-getting-started/main.go
@@ -48,14 +48,22 @@ func main() {
 		   }*/
 		//		id := c.Param("id")  // 여기서 받아옴
 		//name, _ := Dbm.SelectStr("SELECT \"RestaurantName\" FROM \"restaurant\" where \"RestaurantID\" =$1", id)
-		results, _ := Dbm.Select(RestaurantTable{}, "SELECT \"RestaurantName\", \"RestaurantHours\" FROM restaurant where \"RestaurantID\" = 1")
+		//results, _ := Dbm.Select(RestaurantTable{}, "SELECT \"RestaurantName\", \"RestaurantHours\" FROM restaurant where \"RestaurantID\" = 1")
+		mainresults, _ := Dbm.Select(mainTable{}, "SELECT \"mainName\" FROM main where \"mainID\" = 1")
+		
+		var articles []*mainTable
+		for _, r := range mainresults {
+			b := r.(*mainTable)
+			articles = append(articles, b)
+		}
 
+/*
 		var articles []*RestaurantTable
 		for _, r := range results {
 			b := r.(*RestaurantTable)
 			articles = append(articles, b)
 		}
-
+*/
 		//msg.Name = name
 		//msg.Message = "hey"
 		//msg.Number = 123
@@ -68,13 +76,19 @@ func main() {
 
 }
 
+type mainTable struct {
+	mainID       int
+	mainName     string
+}
+
+/*
 type RestaurantTable struct {
 	RestaurantID       int
 	RestaurantName     string
 	RestaurantHours    string
 	RestaurantPosition string
 }
-
+*/
 func InitDB() {
 	// connect to db using standard Go database/sql API
 	// use whatever database/sql driver you wish
--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func main() {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
-	router.GET("/moreJSON/", func(c *gin.Context) { //여기 id던져줄수 있음 /:uid
+	router.GET("/moreJSON1/", func(c *gin.Context) { //여기 id던져줄수 있음 /:uid
 		// You also can use a struct
 		/*
 		   var msg struct {
@@ -48,14 +48,22 @@ func main() {
 		   }*/
 		//		id := c.Param("id")  // 여기서 받아옴
 		//name, _ := Dbm.SelectStr("SELECT \"RestaurantName\" FROM \"restaurant\" where \"RestaurantID\" =$1", id)
-		results, _ := Dbm.Select(RestaurantTable{}, "SELECT \"RestaurantName\", \"RestaurantHours\" FROM restaurant where \"RestaurantID\" = 1")
+		//results, _ := Dbm.Select(RestaurantTable{}, "SELECT \"RestaurantName\", \"RestaurantHours\" FROM restaurant where \"RestaurantID\" = 1")
+		mainresults, _ := Dbm.Select(mainTable{}, "SELECT \"mainName\", \"mainID\" FROM main where \"mainID\" = 1")
+		
+		var articles []*mainTable
+		for _, r := range mainresults {
+			b := r.(*mainTable)
+			articles = append(articles, b)
+		}
 
+/*
 		var articles []*RestaurantTable
 		for _, r := range results {
 			b := r.(*RestaurantTable)
 			articles = append(articles, b)
 		}
-
+*/
 		//msg.Name = name
 		//msg.Message = "hey"
 		//msg.Number = 123
@@ -68,13 +76,19 @@ func main() {
 
 }
 
+type mainTable struct {
+	mainID       int
+	mainName     string
+}
+
+/*
 type RestaurantTable struct {
 	RestaurantID       int
 	RestaurantName     string
 	RestaurantHours    string
 	RestaurantPosition string
 }
-
+*/
 func InitDB() {
 	// connect to db using standard Go database/sql API
 	// use whatever database/sql driver you wish
@@ -118,3 +132,4 @@ func checkErr(err error, msg string) {
 		log.Fatalln(msg, err)
 	}
 }
+
